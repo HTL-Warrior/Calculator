9885f1d4cc446b46fc3ccc148c8dbad4298ceab9
Andri <AndHep18@htl-shkoder.com>
2024-12-17 09:51:59 +0100

    Add test hinzüfügen

---
+++ src/main/java/org/example/CalculatorTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    void add() {
+        assertEquals(7, calculator.add(3, 4));
+        assertNotEquals(9, calculator.add(5, 5));
+    }
+}
